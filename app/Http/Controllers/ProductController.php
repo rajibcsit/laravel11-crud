69abd226b588df44db940102b6e101ad080aa3ed
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-05-08 10:40:47 +0600

    create Product Controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //this method show products page
+    public function index(){
+
+    }
+    
+    //this method create a product 
+    public function create(){
+
+    }
+
+    //this method store a product 
+    public function store(){
+    
+    }
+
+    //this method edit a product 
+    public function edit(){
+
+    }
+
+    //this method update a product 
+    public function update(){
+
+    }
+
+    //this method delete a product 
+    public function destroy() {
+
+    }
+}
